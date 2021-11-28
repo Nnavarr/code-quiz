c7f3a463a37296829b1b1a996a1a8f20b1222d30
eon <noe.navarro1019@gmail.com>
2021-11-28 15:25:56 -0700

    established timer functionality for Start Quiz button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-// delcaration of variables
-var timer = Date.now() + 6000
-console.log(timer);
-console.log(Date.now());
+// establish variables
+var startButtonEL = document.getElementById('startButton');
 
+function countDown() {
+    var timer = 10
+    var updateTimer = setInterval(function() {
+        // decrease timer by 1 second
+        timer -= 1;
+        console.log(timer);
+        // if timer = 0, stop the quiz
+        if (timer === 0){
+            alert('Time is up!');
+            clearInterval(updateTimer);
+        }
+    }, 1000)
+}
+
+// event listeners
+startButtonEL.addEventListener('click', countDown);
