5ac142e3364a9e52a519cade426b3e2b775e3d0b
eon <noe.navarro1019@gmail.com>
2021-11-28 16:49:02 -0700

    added string interpolation for the innerHTML attribute of timerEl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // establish variables
 var startButtonEL = document.getElementById('startButton');
+var timerEl = document.getElementById('timer');
 
 function countDown() {
     var timer = 10
     var updateTimer = setInterval(function() {
         // decrease timer by 1 second
         timer -= 1;
-        console.log(timer);
+        timerEl.innerHTML = `Time: ${timer}`;
         // if timer = 0, stop the quiz
         if (timer === 0){
             alert('Time is up!');
